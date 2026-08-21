0b353dbd52c809ad2f6995f9d0b1b86e4321ff90
Simone Siega <simonesiega1@gmail.com>
2026-08-22 00:02:48 +0200

    feat: add title and fps cap

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,11 @@ int main()
 {
     constexpr int windowWidth = 1280;
     constexpr int windowHeight = 720;
+    constexpr int targetFps = 60;
+    constexpr const char* windowTitle = "Fluid Simulation";
 
-    InitWindow(windowWidth, windowHeight, "Fluid Simulation");
+    InitWindow(windowWidth, windowHeight, windowTitle);
+    SetTargetFPS(targetFps);
 
     Image windowIcon = LoadImage("assets/icons/logo-32.png");
     if (IsImageValid(windowIcon))
@@ -18,6 +21,11 @@ int main()
     {
         BeginDrawing();
         ClearBackground(BLACK);
+        DrawText(windowTitle, 16, 16, 20, LIGHTGRAY);
+
+        const char* fpsText = TextFormat("%d FPS", GetFPS());
+        const int fpsTextWidth = MeasureText(fpsText, 20);
+        DrawText(fpsText, GetScreenWidth() - fpsTextWidth - 16, 16, 20, LIGHTGRAY);
         EndDrawing();
     }
 
